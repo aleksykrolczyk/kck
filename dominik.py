a86f5080ed5ed3fca669caffd21b5dfdc9ba0854
Dominos1234 <52161775+Dominos1234@users.noreply.github.com>
2019-12-03 00:40:23 +0100

    Update dominik.py

--- dominik.py
+++ dominik.py
@@ -2,131 +2,108 @@ from skimage import morphology
 import numpy as np
 import cv2
 
+
 def order_points(pts):
-    # initialzie a list of coordinates that will be ordered
-    # such that the first entry in the list is the top-left,
-    # the second entry is the top-right, the third is the
-    # bottom-right, and the fourth is the bottom-left
+
     rect = np.zeros((4, 2), dtype="float32")
 
-    # the top-left point will have the smallest sum, whereas
-    # the bottom-right point will have the largest sum
+
     s = pts.sum(axis=1)
     rect[0] = pts[np.argmin(s)]
     rect[2] = pts[np.argmax(s)]
 
-    # now, compute the difference between the points, the
-    # top-right point will have the smallest difference,
-    # whereas the bottom-left will have the largest difference
+
     diff = np.diff(pts, axis=1)
     rect[1] = pts[np.argmin(diff)]
     rect[3] = pts[np.argmax(diff)]
 
-    # return the ordered coordinates
+
     return rect
 
 
 def four_point_transform(image, pts):
-    # obtain a consistent order of the points and unpack them
-    # individually
+
     rect = order_points(pts)
     (tl, tr, br, bl) = rect
 
-    # compute the width of the new image, which will be the
-    # maximum distance between bottom-right and bottom-left
-    # x-coordiates or the top-right and top-left x-coordinates
+
     widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
     widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
     maxWidth = max(int(widthA), int(widthB))
 
-    # compute the height of the new image, which will be the
-    # maximum distance between the top-right and bottom-right
-    # y-coordinates or the top-left and bottom-left y-coordinates
+
     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
     maxHeight = max(int(heightA), int(heightB))
 
-    # now that we have the dimensions of the new image, construct
-    # the set of destination points to obtain a "birds eye view",
-    # (i.e. top-down view) of the image, again specifying points
-    # in the top-left, top-right, bottom-right, and bottom-left
-    # order
+
     dst = np.array([
         [0, 0],
         [maxWidth - 1, 0],
         [maxWidth - 1, maxHeight - 1],
         [0, maxHeight - 1]], dtype="float32")
 
-    # compute the perspective transform matrix and then apply it
+
     M = cv2.getPerspectiveTransform(rect, dst)
     warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
 
-    # return the warped image
     return warped
 
+def check_straight(corners,max_x,max_y,min_x,min_y):
+    for i in corners:
+        x, y = i.ravel()
+        if x == max_x:
+            return 1
+        if x == min_x:
+            return 1
+        if y == min_y:
+            return 1
+        if y == max_y:
+            return 1
+    return 0
 
-def calculate_points(im):
-    x1, x2, x3, x4, y1, y2, y3, y4 = -1, -1, -1, -1, -1, -1, -1, -1
-    height, width = im.shape
 
-    for x in range(height):
-        if (x1 != -1 and y1 != -1):
-            break
-        for y in range(width):
-            print(im[x, y])
-            if (im[x, y] == 255):
-                x1, y1 = x, y
-                break
-    for x in range(height - 1, 0, -1):
-        if (x2 != -1 and y2 != -1):
-            break
-        for y in range(width - 1, 0, -1):
-            if (im[x, y] == 255):
-                x2, y2 = x, y
-                break
+def get_corners(im):
 
-    for y in range(width):
-        if (x3 != -1 and y3 != -1):
-            break
-        for x in range(height):
-            if (im[x, y] == 255):
-                x3, y3 = x, y
-                break
+    im = morphology.dilation(im)
 
-    for y in range(width - 1, 0, -1):
-        if (x4 != -1 and y4 != -1):
-            break
-        for x in range(height - 1, 0, -1):
-            if (im[x, y] == 255):
-                x4, y4 = x, y
-                break
+    corners = cv2.goodFeaturesToTrack(im,100,0.08,10)
+    max_x,max_y,min_x,min_y=0,0,10000,10000
+    for i in corners:
+        x, y = i.ravel()
+        cv2.circle(im, (x, y), 3, 255, -1)
+        if x >= max_x:
+            max_x = x
+            pt1 = (x, y)
+        if x <= min_x:
+            min_x = x
+            pt2 = (x, y)
+        if y <= min_y:
+            min_y = y
+            pt3 = (x, y)
+        if y >= max_y:
+            max_y = y
+            pt4 = (x, y)
 
-    pts = np.array([(y1, x1), (y2, x2), (y3, x3), (y4, x4)], dtype="float32")
+    if check_straight(corners,max_x,max_y,min_x,min_y)==1:
+        return []
 
+    cv2.imshow("",im)
+    pts = np.array([pt1,pt2,pt3,pt4], dtype="float32")
+    print(pts)
     return pts
 
 
 
+    cv2.imshow('',im)
+
+def preprocessing():
+    pts = get_corners(image)
+
+    if pts==[]:
+        return image
+    else:
+        return four_point_transform(image,pts)
 
 
-
-image = cv2.imread('images/test4.jpg', cv2.IMREAD_GRAYSCALE)
-
-
-im_bw = cv2.bitwise_not(image)
-thresh,im_bw = cv2.threshold(im_bw, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-
-im_bw = morphology.erosion(im_bw)
-
-# im_bw = cv2.resize(im_bw, (1000,500), interpolation = cv2.INTER_AREA)
-
-
-pts = calculate_points(im_bw)
-
-warped = four_point_transform(image, pts)
-
-
-#cv2.imshow("BlackWhite", im_bw)
-#cv2.imshow("Original", image)
-#cv2.imshow("Warped", warped)
-cv2.waitKey(0)
+image = cv2.imread('test12.png', cv2.IMREAD_GRAYSCALE)
