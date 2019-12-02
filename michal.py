97f1cd2ba54b122578b056acee1a659b0476494b
aleksykrolczyk <aleksykrolczyk@yahoo.pl>
2019-12-02 22:19:22 +0100

    copy

--- michal.py
+++ michal.py
@@ -18,41 +18,36 @@ def sort_contours(cnts, method="left-to-right"):
 
 
 def extract(img_for_box_extraction_path, cropped_dir_path):
+    print('a')
     base_img = cv2.imread(img_for_box_extraction_path, 0)
     base_img = morphology.erosion(base_img)
     threshold, img = cv2.threshold(base_img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     img = 255 - img
-
-    show(img)
-
+    print('a')
     kernel_length = np.array(img).shape[1] // 30
     vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_length))
     horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_length, 1))
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-
+    print('a')
     img_vertical = cv2.erode(img, vertical_kernel, iterations=3)
     img_vertical = cv2.dilate(img_vertical, vertical_kernel, iterations=3)
-    show(img_vertical)
-
+    print('a')
     img_horizontal = cv2.erode(img, horizontal_kernel, iterations=3)
     img_horizontal = cv2.dilate(img_horizontal, horizontal_kernel, iterations=3)
-    show(img_horizontal)
-
+    print('a')
     img = cv2.addWeighted(img_vertical, 0.5, img_horizontal, 0.5, 0.0)
     img = cv2.erode(~img, kernel, iterations=2)
     (thresh, img) = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-
+    print('a')
     img = cv2.erode(img, kernel=np.ones([6,6]), iterations=1)
-
-    show(img)
+    print('a')
     contours, hierarchy = cv2.findContours(
         img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     (contours, boundingBoxes) = sort_contours(contours, method="top-to-bottom")
-
+    print('a')
     idx = 0
-
     model = tf.keras.models.load_model('digits.model')
-
+    print('a')
     def dilate_and_resize(img, steps=2):
         for i in range(steps):
             img = morphology.dilation(img)
@@ -62,7 +57,10 @@ def extract(img_for_box_extraction_path, cropped_dir_path):
         img = img.reshape(28, 28, 1)
         return img
 
-    for c in contours:
+
+    sudoku = []
+    row = []
+    for i, c in enumerate(contours):
         x, y, w, h = cv2.boundingRect(c)
 
         idx += 1
@@ -72,14 +70,13 @@ def extract(img_for_box_extraction_path, cropped_dir_path):
         data = np.array([new_img])
         data = data.astype('float32')
         prediction = model.predict(data)
-        print(np.argmax(prediction[0]))
-        show(new_img)
+        row.insert(0, np.argmax(prediction))
+        if (i+1) % 9 == 0:
+            sudoku.append(row)
+            row = []
+    return np.array(sudoku).T.tolist()
 
-#extract("sudoku.jpeg", "./Cropped/")
-#extract("sudoku1.jpg", "./Cropped/")
-#extract("sudoku2.jpg", "./Cropped/")
-#extract("s1.png", "")
-#extract("s2.png", "")
-#extract("s3.png", "")
-extract("result.jpg", "")
-# extract("s3R.png", "")
+
+sudoku = extract("result.jpg", "")
+for row in sudoku:
+    print(row)
